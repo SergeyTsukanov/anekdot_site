6d574489478322d4b26031251d82b2d73613ce17
DESKTOP-3P3DJAQ\justpassion <zenomonster@gmail.com>
2021-02-04 14:31:36 +0200

    header

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Header from '../Header';
 
 function App() {
   return (
     <div className="App">
- 
+      <Header name='nigga'/>
         Ну здарова bandits!1234
        
     </div>
---
+++ src/Header/Header.js
@@ -0,0 +1,7 @@
+import './Header.css'
+
+function Header(props){
+    return <div>head {props.name}</div>;
+}
+
+export default Header; 
