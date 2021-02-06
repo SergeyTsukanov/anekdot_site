538e857f3e7a881ab82c69c0057d1db52bf5ce3a
DESKTOP-3P3DJAQ\justpassion <zenomonster@gmail.com>
2021-02-06 15:25:29 +0200

    header_comp_v1

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,13 +2,14 @@ import styles from './Header.module.css'
 
 export const Header = (props) => {
     return <header className={styles.header}>
-        <div className ={styles.logo_container}>
-            <span class={styles.logo}>Anekdotiki</span>
-        </div>
-        
-        <div class={styles.hright}>
-            <a class={styles.button_active} href="#signin">Sign In</a>
-            <a class={styles.button_active} href="signup">Sign Up</a>
+        <div className={styles.container}>
+            <div className={styles.logo_container}>
+                <h1 class={styles.logo}>Jokes.com</h1>
+            </div>
+            <div className={styles.button_container}>
+                <a class={styles.button} href="#signin">Sign In</a>
+                <a class={styles.button} href="signup">Sign Up</a>
+            </div>
         </div>
     </header>
 }
