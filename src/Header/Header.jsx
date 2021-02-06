606a1b85b0b078bae33531374fd226b2e58a168a
SergeyTsukanov <sergeyidus@gmail.com>
2021-02-06 13:49:51 +0300

    header changes

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,7 +2,10 @@ import styles from './Header.module.css'
 
 export const Header = (props) => {
     return <header className={styles.header}>
-        <span class={styles.logo}>Anekdotiki</span>
+        <div className ={styles.logo_container}>
+            <span class={styles.logo}>Anekdotiki</span>
+        </div>
+        
         <div class={styles.hright}>
             <a class={styles.button_active} href="#signin">Sign In</a>
             <a class={styles.button_active} href="signup">Sign Up</a>
