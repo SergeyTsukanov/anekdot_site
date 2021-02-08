98f0af6d99a0b211acefa1069d2fefb93c80f636
SergeyTsukanov <sergeyidus@gmail.com>
2021-02-08 13:44:36 +0300

    post style v1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import {Header} from "./Header/Header.jsx"
- 
-
+import {Post} from "./elements/Post"
+import { BrowserRouter, NavLink, Route, Switch } from "react-router-dom";
+import {MainPage} from "./MainPage/MainPage.jsx"
+import { AboutPage} from "./About/AboutPage.jsx";
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Header/>
-        Ну здарова
+      <Switch>
+        <Route path ="/" exact component={MainPage}/>
+        <Route path ="/about" component ={AboutPage}/>
        
+       </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/MainPage/MainPage.jsx
@@ -0,0 +1,9 @@
+import { Post } from "../elements/Post";
+import styles from "./MainPage.module.css";
+ 
+export const MainPage =(props) =>{
+    return<div>
+         <Post/>
+         
+    </div>
+}
